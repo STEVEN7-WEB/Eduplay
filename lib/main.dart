22c2bede3ce68112f03ada079a5050e8b47f1638
Steven Martinez <21690067@tecvalles.mx>
2026-04-25 01:41:22 -0600

    cAMBIO

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/auth/login_screen.dart';
-import 'screens/home/home_screen.dart'; // <-- Importamos la pantalla de materias real
+import 'screens/home/home_screen.dart'; // 
 import 'services/neon_db_service.dart';
 
 void main() async {
-  // 1. Preparamos el motor de Flutter
+  //Preparamos el motor de Flutter
   WidgetsFlutterBinding.ensureInitialized(); 
   
-  // 2. Revisamos si hay una sesión guardada en el celular
+  //Revisamos si hay una sesión guardada en el celular
   final prefs = await SharedPreferences.getInstance();
   final bool yaInicioSesion = prefs.getBool('sesion_iniciada') ?? false;
 
-  // 3. Arrancamos la app pasando el dato de la sesión
+  //Arrancamos la app pasando el dato de la sesión
   runApp(EduplayApp(iniciarDirecto: yaInicioSesion));
 }
 
@@ -28,11 +28,8 @@ class EduplayApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'ComicSans', // O la fuente que prefieras para niños
+        fontFamily: 'ComicSans',
       ),
-      // --- AQUÍ ESTÁ EL CAMBIO CLAVE ---
-      // Si ya inició sesión, va directo a HomeScreen (Materias). 
-      // Si no, va a LoginScreen.
       home: iniciarDirecto ? const HomeScreen() : const LoginScreen(), 
     );
   }
